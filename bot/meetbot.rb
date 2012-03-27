154a25a7fa82eefa0226d8607823ba5b45bb462b
root <root@vm101-103.sc01.thoughtworks.com>
2012-03-27 12:01:14 +0530

    made small changes to meetbot

--- bot/meetbot.rb
+++ bot/meetbot.rb
@@ -4,11 +4,12 @@ require 'fileutils'
 
 def create()
 	$filename = Time.new.to_s.gsub(/\s+/, "-")
-	$path = "/root/meetinglog/#{$filename}.txt"
+	$path = "/var/www/html/meetinglog/#{$filename}.txt"
 	myfile= File.new($path, "w")
 	myfile.puts("Topic:")
 	myfile.puts("Info:")
 	myfile.puts("Action:")
+	myfile.chmod( 0755 )
 	myfile.close
 end
 
@@ -39,31 +40,59 @@ bot = Cinch::Bot.new do
     c.nick = "meetbot"
   end
 
+$mip = false
+
   on :message, "!startmeeting" do |m|
-    create()
-    m.reply "Meeting Started: #{Time.new.to_s}. Chair is: #{m.user.nick}"
-    m.reply "Meeting Commands:\n !topic -to set the meeting topic \n !info -for any relevent discussion regarding meeting\n !action -to set action item\n !endmeeting -to end the meeting"
+    	if $mip == false
+    	create()
+    	m.reply "Meeting Started: #{Time.new.to_s}. Chair is: #{m.user.nick}"
+    	m.reply "Meeting Commands:\n !topic -to set the meeting topic \n !info -for any relevent discussion regarding meeting\n !action -to set action item\n !endmeeting -to end the meeting"
+    	$mip = true
+    	else 
+    	m.reply "!!ERROR::One meeting is already running, please do !ticket help for more options."
+    	end
+  end
+
+  on :message, "!meeting help" do |m|
+    m.reply "Meeting Commands:\n!startmeeting - to start the meeting \n !topic -to set the meeting topic \n !info -for any relevent discussion regarding meeting\n !action -to set action item\n !endmeeting -to end the meeting"
   end
 
   on :message, /^!topic (.*)/  do |m, topic|
-    m.reply "Meeting Topic: #{topic}"
-    update('Topic:', topic)
+   	if $mip == true
+    	m.reply "Meeting Topic: #{topic}"
+    	update('Topic:', topic)
+  	else
+   	m.reply "!!ERROR: No meeting running, please do !ticket help for more options."
+   	end
   end
 
   on :message, /^!info (.*)/ do |m, info|
-    inf =  "#{m.user.nick}: #{info}\n"
-    update('Info:', inf)
+    	if $mip == true
+	inf =  "#{m.user.nick}: #{info}\n"
+    	update('Info:', inf)
+	else
+	m.reply "!!ERROR: No meeting running, please do !ticket help for more options."
+	end
   end
  
   on :message, /^!action (.*)/ do |m, action|
-   act = "#{m.user.nick}: #{action}\n"
-   update('Action:', act)
+	if $mip == true
+	act = "#{m.user.nick}: #{action}\n"
+   	update('Action:', act)
+	else
+	m.reply "!!ERROR: No meeting running, please do !ticket help for more options."
+	end
   end 
 
   on :message, "!endmeeting" do |m|
    # myfile.close
-    m.reply "Meeting ends"
-    m.reply "Minutes are located at:"
+   	if $mip == true
+    	m.reply "Meeting ends, Minutes are stored as #{$filename}"
+    	m.reply "Minutes are located at: http://10.10.101.103/meetinglog/"
+        $mip = false
+	else
+	m.reply "!!ERROR: No meeting running, please do !ticket help for more options."
+	end
   end 
 
 end 
